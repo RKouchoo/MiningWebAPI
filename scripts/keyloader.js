f871e7a853dc585cb9c3ad5aa0c7cd751f58df40
RKouchoo <r.kouchoo1@gmail.com>
2017-10-30 12:35:07 +1100

    created a key. wrote key loader code
    wrote keyloader code and created the keys directory. The loader will return text that can be used with coinHive.

--- scripts/keyloader.js
+++ scripts/keyloader.js
@@ -1,1 +1,21 @@
-// JavaScript Document
+var apiKey = '';
+var apiKeyLocation = 
+
+readKeyFile("./keys/key.apikey");
+
+function readKeyFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4) {
+			
+            if(rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                apiKey = allText;
+            }
+        }
+    }
+    rawFile.send(null);
+}
